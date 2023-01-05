ad40198edbd01faceb94d96c3cebfbb5e8430fe1
Vigilante-Ops <amandixit99@outlook.com>
2023-01-05 11:45:24 +0530

    byname autowiring Check spring.xml for lookup.

--- src/org/vigilantex/demo/Triangle.java
+++ src/org/vigilantex/demo/Triangle.java
@@ -1,24 +1,44 @@
 package org.vigilantex.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Triangle
 {
 
 
-    private List<Point> points;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
-    public List<Point> getPoints() {
-        return points;
+    public Point getPointA() {
+        return pointA;
     }
 
-    public void setPoints(List<Point> points) {
-        this.points = points;
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
     }
 
     public void draw()
     {
-       points.forEach(point-> System.out.println(point.getX()+" "+point.getY()));
-
+        System.out.println(pointA.getX()+" "+ pointA.getY());
+        System.out.println(pointB.getX()+" "+ pointB.getY());
+        System.out.println(pointC.getX()+" "+ pointC.getY());
     }
 }
